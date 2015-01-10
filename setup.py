75e4f477962413de0ca1ce5eccc035758c4d98a8
Kishin Yagami <k.yagami.suou@gmail.com>
2015-01-10 13:38:41 +0900

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,21 @@ setup(
     name='ansible-playbook-debugger',
     version=__version__,
     description='Debugger for Ansible Playbook',
+    long_description="""
+Although Ansible is a powerful tool for configuration management, debugging its
+playbook is often bothersome. One of its reasons is an error message from
+ansible may not include enough information for debug. For example, variables and
+task's keywords are not included usually. Another reason is the time it takes to
+run a playbook. It may take a long time, and you may need to run the playbook
+several times to get more details and to see that the bug is fixed.
+
+ansible-playbook-debugger is the debugger to facilitate such a bothersome work.
+When a task fails, the debugger is invoked automatically. Then you can check
+the module's arguments, variables, keywords, and so on. Furthermore, you can
+change the module's arguments to fix a bug, and re-execute the task. If the
+re-execution is successful, the playbook continues to run as if there was no
+error.
+""",
     classifiers=classifiers,
     author='Kishin Yagami',
     author_email='k.yagami.suou@gmail.com',
