6be80024d83e55155300cc2f82ebb2036e051ac3
Kishin Yagami <k.yagami.suou@gmail.com>
2016-03-16 11:41:20 +0900

    Support continue/redo command

--- strategy_plugins/debug.py
+++ strategy_plugins/debug.py
@@ -13,6 +13,16 @@ except ImportError:
     display = Display()
 
 
+class NextAction(object):
+    """ The next action after an interpreter's exit. """
+    REDO = 1
+    CONTINUE = 2
+    EXIT = 3
+
+    def __init__(self, result=EXIT):
+        self.result = result
+
+
 class StrategyModule(linear.StrategyModule, StrategyBase):
     # Usually inheriting linear.StrategyModule is enough. However, StrategyBase class must be
     # direct ancestor to be considered as strategy plugin, and so we inherit the class here.
@@ -26,20 +36,32 @@ class StrategyModule(linear.StrategyModule, StrategyBase):
         StrategyBase._queue_task(self, host, task, task_vars, play_context)
 
     def _process_pending_results(self, iterator, one_pass=False):
-        results = super(StrategyModule, self)._process_pending_results(iterator, one_pass)
-        if self._need_debug(results):
-            dbg = Debugger(self, results)
+        results = StrategyBase._process_pending_results(self, iterator, one_pass)
+
+        while self._need_debug(results):
+            next_action = NextAction()
+            dbg = Debugger(self, results, next_action)
             dbg.cmdloop()
+
+            if next_action.result == NextAction.REDO:
+                StrategyBase._queue_task(self, self.curr_host, self.curr_task, self.curr_task_vars, self.curr_play_context)
+                results = StrategyBase._process_pending_results(self, iterator, one_pass)
+            elif next_action.result == NextAction.CONTINUE:
+                break
+            elif next_action.result == NextAction.EXIT:
+                exit(1)
+
         return results
 
     def _need_debug(self, results):
         return reduce(lambda total, res : res.is_failed() or res.is_unreachable() or total, results, False)
 
+
 class Debugger(cmd.Cmd):
     prompt = '(debug) '  # debugger
     prompt_continuous = '> '  # multiple lines
 
-    def __init__(self, strategy_module, results):
+    def __init__(self, strategy_module, results, next_action):
         # cmd.Cmd is old-style class
         cmd.Cmd.__init__(self)
 
@@ -50,6 +72,7 @@ class Debugger(cmd.Cmd):
         self.scope['host'] = strategy_module.curr_host
         self.scope['result'] = results[0]._result
         self.scope['results'] = results  # for debug of this debugger
+        self.next_action = next_action
 
     def cmdloop(self):
         try:
@@ -62,9 +85,22 @@ class Debugger(cmd.Cmd):
 
     def do_quit(self, args):
         display.display('aborted')
+        self.next_action.result = NextAction.EXIT
         return True
 
     do_q = do_quit
+
+    def do_continue(self, args):
+        self.next_action.result = NextAction.CONTINUE
+        return True
+
+    do_c = do_continue
+
+    def do_redo(self, args):
+        self.next_action.result = NextAction.REDO
+        return True
+
+    do_r = do_redo
 
     def evaluate(self, args):
         try:
